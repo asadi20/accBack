28a3c0d4d1a8a8627514a38abce94bb5ee3b2303
asadi20 <asadi20@gmail.com>
2026-07-27 22:06:30 +0330

    style(rbac): format PermissionController for better readability

--- app/Http/Controllers/Api/RBAC/PermissionController.php
+++ app/Http/Controllers/Api/RBAC/PermissionController.php
@@ -1,81 +1,81 @@
-    <?php
+<?php
 
-    namespace App\Http\Controllers\Api\RBAC;
+namespace App\Http\Controllers\Api\RBAC;
 
-    use App\Http\Controllers\Controller;
-    use Illuminate\Http\Request;
-    use Illuminate\Validation\Rule;
-    use Spatie\Permission\Models\Permission;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Spatie\Permission\Models\Permission;
 
-    class PermissionController extends Controller
+class PermissionController extends Controller
+{
+    public function index()
     {
-        public function index()
-        {
-            $perms = Permission::all();
-            return response()->json([
-                'success' => true,
-                'data' => $perms,
-                'message' => 'All permissions retrieved successfully.'
-            ], 200);
-        }
+        $perms = Permission::all();
+        return response()->json([
+            'success' => true,
+            'data' => $perms,
+            'message' => 'All permissions retrieved successfully.'
+        ], 200);
+    }
 
-        public function store(Request $request)
-        {
-            $validated = $request->validate([
-                'name' => [
-                    'required',
-                    'string',
-                    'max:255',
-                    Rule::unique('permissions')->where(function ($query) use ($request) {
-                        return $query->where('guard_name', $request->guard_name);
-                    }),
-                ],
-                'guard_name' => 'required|string'
-            ]);
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('permissions')->where(function ($query) use ($request) {
+                    return $query->where('guard_name', $request->guard_name);
+                }),
+            ],
+            'guard_name' => 'required|string'
+        ]);
 
-            $perm = Permission::create($validated);
+        $perm = Permission::create($validated);
 
-            return response()->json([
-                'success' => true,
-                'data' => $perm,
-                'message' => 'a permission created successfully.'
-            ], 201);
-        }
+        return response()->json([
+            'success' => true,
+            'data' => $perm,
+            'message' => 'a permission created successfully.'
+        ], 201);
+    }
 
-        public function show($id)
-        {
-            $perm = Permission::findOrFail($id);
-            return response()->json([
-                'success' => true,
-                'data' => $perm,
-                'message' => 'requested permission retrieved successfully.',
-            ]);
-        }
+    public function show($id)
+    {
+        $perm = Permission::findOrFail($id);
+        return response()->json([
+            'success' => true,
+            'data' => $perm,
+            'message' => 'requested permission retrieved successfully.',
+        ]);
+    }
 
-        public function update(Request $request, $id)
-        {
-            $perm = Permission::findOrFail($id);
+    public function update(Request $request, $id)
+    {
+        $perm = Permission::findOrFail($id);
 
-            $validated = $request->validate([
-                'name' =>[
-                    'required',
-                    'string',
-                    'max:255',
-                    Rule::unique('permissions','name')
+        $validated = $request->validate([
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('permissions', 'name')
                     ->ignore($id)
                     ->where(function ($query) use ($request) {
                         return $query->where('guard_name', $request->guard_name);
                     }),
-                ],
-                'guard_name'=> 'required|string|max:255',
-            ]);
+            ],
+            'guard_name' => 'required|string|max:255',
+        ]);
 
-            $perm->update($validated);
+        $perm->update($validated);
 
-            return response()->json([
-                'success' => true,
-                'data' => $perm,
-                'message' => 'permission updated successfully.',
-            ], 200);
-        }
+        return response()->json([
+            'success' => true,
+            'data' => $perm,
+            'message' => 'permission updated successfully.',
+        ], 200);
     }
+}
